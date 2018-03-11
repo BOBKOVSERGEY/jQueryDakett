80508ffa7d374e82b7f084344a401deb35ec91a1
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-03-11 12:08:34 +0300

    Поиск элементов по их порядковому номеру

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var bs = require("browser-sync").create();
 
 //тут
-bs.watch(['filtrs/*.*']).on("change", bs.reload);
+bs.watch(['search-elements/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  server: "./filtrs"
+  server: "./search-elements"
 });
