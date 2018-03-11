1dc8e0ccd3f390bf3b18813145aec00e7d7f506f
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-03-11 11:29:33 +0300

    Animate

--- vizualnye-effecty/index.js
+++ vizualnye-effecty/index.js
@@ -5,7 +5,16 @@ $(function () {
   elLi.hide().each(function (index) {
     $(this).delay(700*index).fadeIn(700);
   });
-  elLi.on('click', function () {
+  /*elLi.on('click', function () {
     $(this).fadeOut(700);
+  });*/
+
+  elLi.on('click', function () {
+    $(this).animate({
+      opacity: 0.0,
+      paddingLeft: '+=80'
+    }, 500, function () {
+      $(this).remove();
+    })
   })
 });
