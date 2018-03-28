d8f0fdbf2243bfde7e221cba1a9eb2583b2f4741
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-03-28 11:57:07 +0300

    Big example ajax

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var bs = require("browser-sync").create();
 
 //тут
-bs.watch(['ajax-jquery/example/*.*']).on("change", bs.reload);
+bs.watch(['ajax-jquery/bigexample/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  server: "./ajax-jquery/example/"
+  server: "./ajax-jquery/bigexample/"
 });
